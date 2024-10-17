078b9e4ec8cb9670cc087fb9347b2a2568016694
ZhdanovVlad <gtr35skyline32@gmail.com>
2024-10-17 21:04:36 +1000

    Add files via upload

---
+++ papa.js
@@ -0,0 +1,39 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// let result = (a + b)*c
+
+// console.log(result);
+
+
+
+
+
+// const numbers = [2, 9.4, 18.1, 21, 46.3]
+// const result = numbers.reduce((sum, numbers)=> sum + numbers,)
+
+// console.log(result)
+
+
+
+
+
+
+// function inMetr(cm){
+//     return cm/100;
+// }
+
+// console.log(inMetr(200))
+
+
+
+
+
+// function convertRtoc(rur){
+//     const change = 12
+//     const cny = rur * change
+//     return cny.riFixed(2)
+// }
+
+// console.log(convertRtoc(150))
